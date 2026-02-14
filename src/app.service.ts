a35a8497cfa8b46402a33b11d926200c9916fba4
Lázaro Campos <lazaromanuel65@gmail.com>
2026-02-14 15:20:10 -0500

    add healthcheck

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+type HealthcheckResponse = {
+  status: 'ok';
+  timestamp: string;
+  uptime: number;
+};
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealthcheck(): HealthcheckResponse {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    };
   }
 }
